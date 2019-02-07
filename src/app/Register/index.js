4e0e106ec9100ff70c88ff5ab9b4e69cbd3f1b02
Ezra Singh <singhezra@gmail.com>
2019-02-07 14:12:30 -0500

    Registration view UX

--- src/app/Register/index.js
+++ src/app/Register/index.js
@@ -1,18 +1,18 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import Form from './components/Form'
 
 const Register = () => {
     return(
-        <Fragment>
-            <h2>Register</h2>
-            <div>
-                <Form/>
+        <div className="register overlay flex h-center v-center">
+            <div className="form box">
+                <h2>Register</h2>
+                    <Form/>
+                <Link to="/">
+                    Already have an account?
+                </Link>
             </div>
-            <Link to="/">
-                Already have an account?
-            </Link>
-        </Fragment>
+        </div>
     )
 }
 
