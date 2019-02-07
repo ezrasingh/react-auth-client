09120b86ee3715f1f696ae4497b56f4bb257e61d
Ezra Singh <singhezra@gmail.com>
2019-02-07 11:06:43 -0500

    Refactored profile with container/component pattern

--- src/app/Profile/index.js
+++ src/app/Profile/index.js
@@ -1,51 +1,14 @@
-import React, { Component, Fragment } from 'react'
-import { Link } from 'react-router-dom'
-import { isEmpty } from 'lodash'
-import { connect } from 'react-redux'
-import { profile } from 'actions/user'
+import React from 'react'
+import Navigation from './components/Navigation'
+import { default as ProfileCard } from './containers/CardContainer' 
 
-class Profile extends Component{
-    componentWillMount(){
-        this.props.load()
-    }
-    renderProfile(){
-        const { profile } = this.props
-        if(isEmpty(profile)){
-            return <span>Please create a profile.</span>
-        }
-        else{
-            return(
-                <Fragment>
-                    <strong>Name</strong> : { 'name' in profile && profile.name }
-                    <br/>
-                </Fragment>
-            )
-        }
-    }
-    render(){
-        const { email } = this.props
-        if(!email){
-            return <em>Loading....</em>
-        }
-        return(
-            <div>
-                <strong>Email</strong> : { email }
-                <br/>
-                <h2>Profile</h2>
-                {this.renderProfile()}
-                <Link to="/profile/update">Update Profile</Link>
-                <Link to="/logout">Logout</Link>
-            </div>
-        )
-    }
+const Profile = () => {
+    return(
+        <div className="profile">
+            <Navigation/>
+            <ProfileCard/>
+        </div>
+    )
 }
 
-const mapStateToProps = state => {
-    return { ...state.user }
-}
-
-const mapDispatchToProps = dispatch => {
-    return { load: () => dispatch(profile()) }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Profile)
+export default Profile
