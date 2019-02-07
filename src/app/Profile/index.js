b0c04e18e6218e7f9c764639b3fe6bae5e2ce680
Ezra Singh <singhezra@gmail.com>
2019-02-07 18:12:34 -0500

    Refactored Profile view to use Navbar component

--- src/app/Profile/index.js
+++ src/app/Profile/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navigation from './components/Navigation'
+import Navbar from 'app/components/Navbar'
 import { default as ProfileCard } from './containers/CardContainer' 
 
 const Profile = () => {
     return(
         <div className="profile overlay">
-            <Navigation/>
+            <Navbar/>
             <div id="card-container" className="flex v-center h-center">
                 <ProfileCard/>
             </div>
