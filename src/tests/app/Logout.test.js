f7d086b611a98bb041bc1294e23b3f68a33325ad
Ezra Singh <singhezra@gmail.com>
2019-03-05 13:36:05 -0500

    Updated Logout view test

--- src/tests/app/Logout.test.js
+++ src/tests/app/Logout.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 import { shallow } from 'enzyme'
+import Loader from 'app/components/Loader'
 import { LogoutComponent } from 'app/Logout'
 
 const logout = jest.fn()
@@ -11,9 +12,9 @@ describe('<Logout/>', () => {
         shallow(<Logout isLoggedIn/>)
         expect(logout).toHaveBeenCalledTimes(1)
     })
-    it('should render logging out message', () => {
+    it('should render loader while logging out', () => {
         const component = shallow(<Logout isLoggedIn/>)
-        expect(component.dive().text()).toBe('Logging out...')
+        expect(component.dive().equals(<Loader/>)).toBe(true)
     })
     it('should redirect a user', () => {
         const component = shallow(<Logout isLoggedIn={false}/>)
