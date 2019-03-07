2052699619f3b5b97e4cb47e2dd63948a9bbeb84
Ezra Singh <ezra@liontech.nyc>
2019-03-07 14:36:50 -0500

    Version 1.1.0

---
+++ build/precache-manifest.18fb2065970ff45490c36f6526851f8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "467b02200a2db72cf5b4",
+    "url": "/static/js/main.467b0220.chunk.js"
+  },
+  {
+    "revision": "e9f7fb29adc2acc86fdc",
+    "url": "/static/js/1.e9f7fb29.chunk.js"
+  },
+  {
+    "revision": "467b02200a2db72cf5b4",
+    "url": "/static/css/main.63588e15.chunk.css"
+  },
+  {
+    "revision": "593bdbd6d4c7a499c90cfa43d1a9a1bf",
+    "url": "/index.html"
+  }
+];
