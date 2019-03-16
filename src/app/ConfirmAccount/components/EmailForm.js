8807d316250a28fdae0fba2425551f99d7e94757
Ezra Singh <singhezra@gmail.com>
2019-03-16 19:58:01 -0400

    Fixed some text within the form

--- src/app/ConfirmAccount/components/EmailForm.js
+++ src/app/ConfirmAccount/components/EmailForm.js
@@ -6,14 +6,14 @@ const EmailForm = ({ handleSubmit }) => {
     return(
         <form onSubmit={handleSubmit}>
             <header>
-                <h3>Account Recovery</h3>
-                <p>Resend recovery link via email</p>
+                <h3>Account Confirmation</h3>
+                <p>Resend confirmation link via email</p>
             </header>
 
             <label htmlFor="email">Email</label>
             <input id="email" name="email" type="email" required/>
 
-            <input className="btn" type="submit" value="Send Recovery Link"/>
+            <input className="btn" type="submit" value="Send Confirmation Link"/>
             <div className="prompt">
                 <Link className="link" to="/register">Go back to registration</Link>
             </div>
